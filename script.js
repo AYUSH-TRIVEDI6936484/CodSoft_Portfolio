6b96a8bb48282d4ccb21801830aac215ff748258
ayush.trivedi0511@gmail.com <its.trivedi05@h=gmail.com>
2023-07-15 20:27:38 +0530

    restructuring the entire website

---
+++ script.js
@@ -0,0 +1,15 @@
+// let typed= new Typed('.auto-input',{
+//     strings: ['Front-End Developer', 'Freelancer','Competitive Programmer'],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 2000,
+//     loop: true,
+// })
+
+$(document).ready(function() {
+    var options = {
+    strings: ["hope", "family"],
+    typeSpeed: 40
+    }
+    var typed = new Typed(".auto-input", options);
+    });
