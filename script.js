a94b96953b7e944a85e984b4136f4aebc6c1e547
ayush.trivedi0511@gmail.com <its.trivedi05@h=gmail.com>
2023-07-17 23:00:36 +0530

    creating skills sections

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let typed= new Typed('.auto-input',{
-    strings: ['Front-End Developer!', 'Freelancer!','Competitive Programmer!'],
+let typed = new Typed('.auto-input', {
+    strings: ['Front-End Developer!', 'Freelancer!', 'Competitive Programmer!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 200,
@@ -8,19 +8,19 @@ let typed= new Typed('.auto-input',{
 
 
 let sections = document.querySelectorAll('section');
-let navLinks= document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop -150;
-        let height=sec.offsetHeight;
-        let id= sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset+height){
-            navLinks.forEach(links=> {
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
                 links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
